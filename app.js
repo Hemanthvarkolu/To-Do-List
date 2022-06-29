f07564412ef0a45568f295da294fbb5a586e73da
HemanthVarkolu <hemanthvarkolu@gmail.com>
2022-06-29 15:08:56 +0530

    final1 commit

--- app.js
+++ app.js
@@ -133,11 +133,6 @@ app.get("/:categorytxt",function(req,res){
   });
 });
 
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
